9ba49b083d7e21df2b978ab93e2949b1ffb9bef1
appleshakey <adarshnikhil181@gmail.com>
2024-08-31 09:38:58 +0530

    app developed

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,65 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="">
+      <Navbar />
+      <div className="flex flex-col gap-28 pt-24">
+        <div className="grid grid-cols-2 px-7">
+          <div className="font-staat text-primary flex justify-center items-center text-8xl text-center">
+            Protecting Your Health Through Data
+          </div>
+          <div className="flex justify-center items-center">
+            <img src="./About.png" className="w-[75%] rounded-lg shadow-lg" />
+          </div>
+        </div>
+        <div className="about p-10 flex flex-col gap-7 items-start">
+            <div className="text-5xl font-semibold font-staat text-primary">
+              About Us
+            </div>
+            <div className="text-2xl font-medium font-staat text-primary w-[50vw]">
+            Welcome to HeartSense, a dedicated platform aimed at improving heart health through data-driven insights. Our mission is to collect and analyze critical information that can help identify risk factors for heart disease, enabling better prevention, early detection, and personalized care.
+            </div>
+        </div>
+        <div className="about p-10 flex flex-col gap-7 items-end text-end">
+            <div className="text-5xl font-semibold font-staat text-primary">
+              Our Mission
+            </div>
+            <div className="text-2xl font-medium font-staat text-primary w-[50vw]">
+            Heart disease remains one of the leading causes of death worldwide, but with the right knowledge and tools, many cases can be prevented. Our mission is to empower individuals, healthcare professionals, and researchers by providing a comprehensive database of risk factors related to heart disease. By gathering and analyzing this data, we aim to contribute to more accurate prediction models, ultimately reducing the burden of heart disease.
+            </div>
+        </div>
+        <div className="about p-10 flex flex-col gap-7 items-start text-start">
+            <div className="text-5xl font-semibold font-staat text-primary">
+              What do we do?  
+            </div>
+            <div className="text-2xl font-medium font-staat text-primary w-[50vw]">
+            At HeartSense, we focus on collecting data from a wide range of sources, including lifestyle habits, medical history, genetic information, and environmental factors. We provide a secure and user-friendly platform where users can contribute their data anonymously, knowing that they are playing a vital role in advancing heart disease research.
+            </div>
+        </div>
+        <div className="about p-10 flex flex-col gap-7 items-end text-end">
+            <div className="text-5xl font-semibold font-staat text-primary">
+              Our Commitment
+            </div>
+            <div className="text-2xl font-medium font-staat text-primary w-[50vw]">
+            We are committed to maintaining the highest standards of privacy and data security. All data collected on our platform is anonymized and encrypted, ensuring that your personal information is protected. Our team of experts follows strict ethical guidelines to ensure that the data is used solely for research and development purposes aimed at improving heart health.
+            </div>
+        </div>
+        <div className="flex flex-col justify-center items-center py-24 px-16 text-center gap-10">
+          <div className="grid grid-cols-2 gap-10 shadow-lg p-5">
+              <div className=" text-8xl font-staat text-primary font-bold flex justify-center items-center border-r-4 border-primary">
+                <h1>Join Us in the Fight Against Heart Disease</h1>
+              </div>
+              <div className="text-3xl font-staat font-mediun text-primary flex items-center">
+              By participating in our data collection efforts, you are contributing to a global initiative to combat heart disease. Together, we can create a future where heart disease is detected early and prevented before it becomes life-threatening. Whether you are a patient, a healthcare provider, or simply someone interested in heart health, your contribution matters.
+          </div>
+          </div>
+          <div>
+            <button>Sign Up Now</button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>    
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary": "#29783e"
+      },
+      fontFamily: {
+        "staat": ["Staatliches", "sans-serif"]
+      }
     },
   },
   plugins: [],
